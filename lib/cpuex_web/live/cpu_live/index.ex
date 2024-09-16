4e0c410bdf80012acd9a4c4f379059050babd96d
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-16 21:46:26 +0900

    get_cpuinfo

--- lib/cpuex_web/live/cpu_live/index.ex
+++ lib/cpuex_web/live/cpu_live/index.ex
@@ -4,12 +4,26 @@ defmodule CpuexWeb.CpuLive.Index do
   @impl true
   def mount(_params, _session, socket) do
     Process.send_after(self(), :update, 250)
-    {:ok, socket}
+
+    socket
+    |> assign(cpus: get_cpuinfo())
+    |> ok()
   end
 
   @impl true
   def handle_info(:update, socket) do
     Process.send_after(self(), :update, 250)
-    {:noreply, socket}
+    socket
+    |> assign(cpus: get_cpuinfo())
+    |> noreply()
   end
+
+  def get_cpuinfo do
+    File.read!("/proc/cpuinfo")
+    |> String.split("\n")
+    |> Enum.filter(&String.match?(&1, ~r/cpu MHz/))
+  end
+
+  def ok(socket), do: {:ok, socket}
+  def noreply(socket), do: {:noreply, socket}
 end
