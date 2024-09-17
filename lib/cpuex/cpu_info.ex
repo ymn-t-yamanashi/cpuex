910e46d8ab1204a181a28dfa02e2fcb712162765
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-17 13:39:41 +0900

    線グラフ試作

--- lib/cpuex/cpu_info.ex
+++ lib/cpuex/cpu_info.ex
@@ -17,20 +17,33 @@ defmodule Cpuex.CpuInfo do
     |> String.to_integer()
   end
 
-  def create_graph(cpuinfo) do
+  def create_graph(cpuinfo, history \\ []) do
     Vl.new(width: 800, height: 400)
-    |> create_bar_graph(cpuinfo)
+    |> Vl.concat(
+      [
+        create_bar_graph(cpuinfo),
+        create_line_graph(history |> List.first())
+      ],
+      :vertical
+    )
     |> Vl.Export.to_png()
     |> Base.encode64()
     |> then(&"data:image/png;base64,#{&1}")
   end
 
-  defp create_bar_graph(vl, cpuinfo) do
-    vl
+  defp create_bar_graph(cpuinfo) do
+    Vl.new(width: 800)
     |> Vl.data_from_values(x: 1..32, y: cpuinfo)
     |> Vl.mark(:bar, color: "#ffaaaa", width: 20)
-    |> Vl.encode_field(:x, "x", type: :quantitative)
-    |> Vl.encode_field(:y, "y", type: :quantitative)
+    |> Vl.encode_field(:x, "x", type: :quantitative, scale: %{domain: [0, 32]})
+    |> Vl.encode_field(:y, "y", type: :quantitative, scale: %{domain: [0, 6000]})
   end
 
+  defp create_line_graph(cpuinfo) do
+    Vl.new(width: 800)
+    |> Vl.data_from_values(x: 1..10, y: cpuinfo)
+    |> Vl.mark(:line, color: "#ffaaaa", width: 20)
+    |> Vl.encode_field(:x, "x", type: :quantitative, scale: %{domain: [0, 100]})
+    |> Vl.encode_field(:y, "y", type: :quantitative, scale: %{domain: [0, 6000]})
+  end
 end
--- lib/cpuex_web/live/cpu_live/index.ex
+++ lib/cpuex_web/live/cpu_live/index.ex
@@ -24,12 +24,9 @@ defmodule CpuexWeb.CpuLive.Index do
   defp assign_cpuinfo(socket) do
     cpuinfo = CpuInfo.get_cpuinfo()
 
-    history = Map.get(socket.assigns, :history, [])
-    |> then(& &1 ++ [cpuinfo])
-
-    history
-    |> history_to_list()
-    |> IO.inspect()
+    history =
+      Map.get(socket.assigns, :history, [])
+      |> then(&(&1 ++ [cpuinfo]))
 
     max_mhz =
       Map.get(socket.assigns, :max_mhz, 0)
@@ -38,13 +35,13 @@ defmodule CpuexWeb.CpuLive.Index do
     socket
     |> assign(max_mhz: max_mhz)
     |> assign(history: history)
-    |> assign(graph: CpuInfo.create_graph(cpuinfo))
+    |> assign(graph: CpuInfo.create_graph(cpuinfo, history_to_list(history)))
   end
 
   defp history_to_list(history) do
     history
     |> Enum.zip()
-    |> Enum.map(& Tuple.to_list(&1))
+    |> Enum.map(&Tuple.to_list(&1))
   end
 
   defp update_cpu_info(), do: Process.send_after(self(), :update, @update_time)
