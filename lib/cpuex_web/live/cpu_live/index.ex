4474f72f90a073805339fd3dbb332874beca8aa8
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-17 11:40:59 +0900

    assign_cpuinfo作成

--- lib/cpuex_web/live/cpu_live/index.ex
+++ lib/cpuex_web/live/cpu_live/index.ex
@@ -5,28 +5,32 @@ defmodule CpuexWeb.CpuLive.Index do
   @impl true
   def mount(_params, _session, socket) do
     Process.send_after(self(), :update, 250)
-    cpuinfo = CpuInfo.get_cpuinfo()
-    max_mhz = Enum.max(cpuinfo)
 
     socket
-    |> assign(max_mhz: max_mhz)
-    |> assign(graph: CpuInfo.create_graph(cpuinfo))
+    |> assign_cpuinfo()
     |> ok()
   end
 
   @impl true
   def handle_info(:update, socket) do
     Process.send_after(self(), :update, 250)
-    cpuinfo = CpuInfo.get_cpuinfo()
 
-    max_mhz = Enum.max(cpuinfo ++ [socket.assigns.max_mhz])
+    socket
+    |> assign_cpuinfo()
+    |> noreply()
+  end
+
+  defp assign_cpuinfo(socket) do
+    cpuinfo = CpuInfo.get_cpuinfo()
+    max_mhz = Map.get(socket.assigns, :max_mhz, 0)
+
+    max_mhz = Enum.max(cpuinfo ++ [max_mhz])
 
     socket
     |> assign(max_mhz: max_mhz)
     |> assign(graph: CpuInfo.create_graph(cpuinfo))
-    |> noreply()
   end
 
-  def ok(socket), do: {:ok, socket}
-  def noreply(socket), do: {:noreply, socket}
+  defp ok(socket), do: {:ok, socket}
+  defp noreply(socket), do: {:noreply, socket}
 end
