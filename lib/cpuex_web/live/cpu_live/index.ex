441fb4712e3c8436e3d5e3276f9ee042eda97c03
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-17 12:54:50 +0900

    CpuexWeb.CpuLive.Indexリファクタリング

--- lib/cpuex_web/live/cpu_live/index.ex
+++ lib/cpuex_web/live/cpu_live/index.ex
@@ -1,10 +1,11 @@
 defmodule CpuexWeb.CpuLive.Index do
   alias Cpuex.CpuInfo
   use CpuexWeb, :live_view
+  @update_time 5000
 
   @impl true
   def mount(_params, _session, socket) do
-    Process.send_after(self(), :update, 250)
+    update_cpu_info()
 
     socket
     |> assign_cpuinfo()
@@ -13,7 +14,7 @@ defmodule CpuexWeb.CpuLive.Index do
 
   @impl true
   def handle_info(:update, socket) do
-    Process.send_after(self(), :update, 250)
+    update_cpu_info()
 
     socket
     |> assign_cpuinfo()
@@ -23,15 +24,30 @@ defmodule CpuexWeb.CpuLive.Index do
   defp assign_cpuinfo(socket) do
     cpuinfo = CpuInfo.get_cpuinfo()
 
+    history = Map.get(socket.assigns, :history, [])
+    |> then(& &1 ++ [cpuinfo])
+
+    history
+    |> history_to_list()
+    |> IO.inspect()
+
     max_mhz =
       Map.get(socket.assigns, :max_mhz, 0)
       |> then(&Enum.max(cpuinfo ++ [&1]))
 
     socket
     |> assign(max_mhz: max_mhz)
+    |> assign(history: history)
     |> assign(graph: CpuInfo.create_graph(cpuinfo))
   end
 
+  defp history_to_list(history) do
+    history
+    |> Enum.zip()
+    |> Enum.map(& Tuple.to_list(&1))
+  end
+
+  defp update_cpu_info(), do: Process.send_after(self(), :update, @update_time)
   defp ok(socket), do: {:ok, socket}
   defp noreply(socket), do: {:noreply, socket}
 end
